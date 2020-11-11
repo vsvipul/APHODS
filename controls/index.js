9703576e6f7e26bfd7a520f327e70f825962281b
Swapnil Rustagi <swapnilrustagi97792@gmail.com>
2020-11-11 22:19:22 +0530

    Fetch Dockerfile

--- controls/index.js
+++ controls/index.js
@@ -1,14 +1,25 @@
 var helper = {};
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+let containerId = 0; // How to keep this persistent?
+
 
 helper.mainPage = async(req, res, next) => {
     res.render("index");
 }
 
 helper.handleQuery = async(req, res, next) => {
-    let formdata = req.body;
+    const formdata = req.body;
     console.log(formdata.repoLink);
     console.log(formdata.subdomain);
     console.log(formdata.port);
+    fs.mkdirSync("./containers/"+(containerId+1), {recursive: true});
+    const file = fs.createWriteStream("./containers/"+(containerId+1) + "/Dockerfile");
+    const request = https.get(formdata.repoLink, function(response) {
+        response.pipe(file);
+    });
     res.render('index');
 }
 
