6923662ffaff0f6576a1dac66e5af3cb25fbb29e
Swapnil Rustagi <swapnilrustagi97792@gmail.com>
2020-11-11 21:55:18 +0530

    Fetch data from form

--- controls/index.js
+++ controls/index.js
@@ -4,4 +4,12 @@ helper.mainPage = async(req, res, next) => {
     res.render("index");
 }
 
+helper.handleQuery = async(req, res, next) => {
+    let formdata = req.body;
+    console.log(formdata.repoLink);
+    console.log(formdata.subdomain);
+    console.log(formdata.port);
+    res.render('index');
+}
+
 module.exports = helper;
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var indexControl = require("../controls/index");
 
 router.get("/", indexControl.mainPage);
+router.post("/", indexControl.handleQuery);
 
 module.exports = router;
