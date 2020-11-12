2a5a3fcde5a799d0e5608cca8352fbad6ec8ec2a
Swapnil Rustagi <swapnilrustagi97792@gmail.com>
2020-11-12 11:28:37 +0530

    Docker container setup

--- controls/index.js
+++ controls/index.js
@@ -1,23 +1,29 @@
 var helper = {};
 const https = require('https');
 const fs = require('fs');
+const { exec } = require('child_process');
 
-let containerId = 0; // How to keep this persistent?
-
+let containerId = 0; 
 
 helper.mainPage = async(req, res, next) => {
     res.render("index");
 }
 
 helper.handleQuery = async(req, res, next) => {
+    containerId++;
     const formdata = req.body;
-    console.log(formdata.repoLink);
+    console.log(formdata.dockerfileLink);
     console.log(formdata.subdomain);
     console.log(formdata.port);
-    fs.mkdirSync("./containers/"+(containerId+1), {recursive: true});
-    const file = fs.createWriteStream("./containers/"+(containerId+1) + "/Dockerfile");
-    const request = https.get(formdata.repoLink, function(response) {
+    fs.mkdirSync("./containers/" + containerId, { recursive: true });
+    const file = fs.createWriteStream("./containers/"+containerId + "/Dockerfile");
+    const request = https.get(formdata.dockerfileLink, function(response) {
         response.pipe(file);
+        process.chdir("./containers/"+containerId);
+        const childProcess = exec("docker build -t c"+ containerId + " . && docker run -it c" + containerId);
+        childProcess.stdout.pipe(process.stdout);
+        childProcess.stderr.pipe(process.stderr);
+        process.chdir("../..");
     });
     res.render('index');
 }
