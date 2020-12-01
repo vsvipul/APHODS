3eb3a26644c149fab22294a83454db708c7e16b5
Swapnil Rustagi <swapnilrustagi97792@gmail.com>
2020-12-01 22:49:11 +0530

    Partial integration work

--- controls/index.js
+++ controls/index.js
@@ -9,36 +9,55 @@ helper.mainPage = async(req, res, next) => {
     res.render("index");
 }
 
+const dockerRun = (command, res) => {
+    const childProcess = exec(command, (error, stdout, stderr) => {
+        if (error==null)
+        res.render('postsubmit', {message: 'Container is running!', details: {}});
+        else res.render('postsubmit', {message: 'Docker run failed!', error: {command, stdout, stderr}});
+    });
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stderr.pipe(process.stderr);
+}
+
+
 helper.handleQuery = async(req, res, next) => {
     containerId++;
     const formdata = req.body;
     // console.log(formdata.dockerfileLink);
     // console.log(formdata.subdomain);
     // console.log(formdata.port);
-    // console.log(formdata);
+    console.log(formdata);
     if (formdata.optradio == "dockerfile") {
         // Do this if dockerfile selected
         // Use formdata.dockerfileLink
-        fs.mkdirSync("./containers/" + containerId, { recursive: true });
-        const file = fs.createWriteStream("./containers/"+containerId + "/Dockerfile");
-        const request = https.get(formdata.dockerfileLink, function(response) {
-            response.pipe(file);
-            process.chdir("./containers/"+containerId);
-            const childProcess = exec("docker build -t c"+ containerId + " . && docker run -it c" + containerId);
-            childProcess.stdout.pipe(process.stdout);
-            childProcess.stderr.pipe(process.stderr);
-            process.chdir("../..");
-        });
-        res.render('index');
+        // fs.mkdirSync("./containers/" + containerId, { recursive: true });
+        // const file = fs.createWriteStream("./containers/"+containerId + "/Dockerfile");
+        // const request = https.get(formdata.dockerfileLink, function(response) {
+        //     response.pipe(file);
+        //     process.chdir("./containers/"+containerId);
+        //     const childProcess = exec("docker build -t c"+ containerId + " . && docker run -it c" + containerId);
+        //     childProcess.stdout.pipe(process.stdout);
+        //     childProcess.stderr.pipe(process.stderr);
+        //     process.chdir("../..");
+        // });
     } else if (formdata.optradio == "dockerImage") {
         // Do this if dockerImage selected
         // Use formdata.dockerImage
-        console.log("docker run -dit " + formdata.dockerImage);
-        const childProcess = exec("docker run -dit " + formdata.dockerImage);
-        childProcess.stdout.pipe(process.stdout);
-        childProcess.stderr.pipe(process.stderr);
-
-        // Need to check -it vs -dit (or can be provided as options later).
+        res.render('postsubmit', {message: 'Pulling Docker image'}, () => {
+            let command = "docker run -";
+            command += formdata.optradioRunMode;
+            command += (" -p " + formdata.port);
+            command += (" " + formdata.dockerImage);
+            const pullProcess = exec("docker pull " + formdata.dockerImage, (error, stdout, stderr)=> {
+                if (error==null) {
+                    res.render('postsubmit', {message: 'Starting container'} , () => {
+                        console.log(command);
+                        dockerRun(command, res);
+                    });
+                }
+                else res.render('postsubmit', {message: 'Error pulling image', error: {command: ("docker pull " + formdata.dockerImage) , stdout, stderr}});
+            });
+        });
     }
 }
 
